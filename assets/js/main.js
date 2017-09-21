0874a41225da2868a3a22aad2a97e841a6b982f2
Rejvin <talukdarrejvin@gmail.com>
2017-09-22 02:38:18 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,6 @@
 
     jQuery(document).ready(function($){
 
-    	$(".testimonial-section").owlCarousel({
-		    items: 1,
-		    autoplay: 3000,
-		    margin: 60,
-			loop: true,
-			nav: true,
-			navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
-		    smartSpeed: 800
-		});
-
         /*  Slick Nav Mobile menu  */
 	   $('#menuResponsive').slicknav({
 		   prependTo: "#mobile-menu-wrap",
@@ -27,38 +17,15 @@
     	});
 
 
-       $(".blog-area-slider").owlCarousel({
-	    items: 3,
-	    autoplay: true,
-	    margin: 60,
-		loop: true,
-		nav: true,
-		navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
-	    smartSpeed: 800,
-	    responsive : {
-			0 : {
-				items: 1,
-			},
-			768 : {
-				items: 2,
-			},
-			992 : {
-				items: 3
-			}
-		}
-	});	
-
-
-    $('.slicknav_btn').click(function(){
-     	$(this).fadeOut(300);
-     	$('.menu-close').fadeIn(300);
+       
+     $('.slicknav_btn').click(function(){
+     	$('.menu-close').slideDown(500);
      });
      $('.menu-close').click(function(){
-     	$('.slicknav_nav, .menu-close').fadeOut(300);
-     	$('.slicknav_btn').fadeIn(300);
+     	$('.slicknav_nav, .menu-close').slideUp(500);
      });
      $('.slicknav_btn').click(function(){
-     	$('.slicknav_nav').fadeIn(300);
+     	$('.slicknav_nav').slideDown(500);
      });
 
 
